aaba66824a7221b27d472b8c0471e5a52dd9f043
Stache <stache@stache.cat>
2022-10-30 18:42:18 +0100

    First pre-commit

--- server/nicelan/nicelan/asgi.py
+++ server/nicelan/nicelan/asgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+
 from django.core.asgi import get_asgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nicelan.settings")
 
--- server/nicelan/nicelan/wsgi.py
+++ server/nicelan/nicelan/wsgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nicelan.settings")
 
