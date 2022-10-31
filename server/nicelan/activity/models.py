8cd12d853054225f4737605351d4d69ce823a6d1
Stache <stache@stache.cat>
2022-10-31 15:09:35 +0100

    First draft for models

--- server/nicelan/activity/models.py
+++ server/nicelan/activity/models.py
@@ -1,4 +1,33 @@
 from django.db import models
 
 
-# Create your models here.
+OTH = "OTHER"
+ACTIVITY_CATEGORIES = [
+    (OTH, "Other"),
+]
+
+
+class Activity(models.Model):
+    name = models.CharField(max_length=64)
+    event = models.ForeignKey("event.Event", on_delete=models.CASCADE)
+    category = models.CharField(max_length=8, choices=ACTIVITY_CATEGORIES, default=OTH)  # TODO why?
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    place = models.CharField(max_length=64)  # TODO Model for places choices?
+    team = models.BooleanField(default=False)
+    description = models.CharField(max_length=1024)
+
+
+class Tournament(Activity):
+    points_min = models.IntegerField(default=0)
+    points_per_place = None  # TODO
+    format = None  # TODO
+    bracket = None  # TODO
+
+
+class Animation(Activity):
+    possible_points = models.IntegerField(default=0)  # TODO
+
+
+class Other(Activity):
+    pass
--- server/nicelan/partaker/models.py
+++ server/nicelan/partaker/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
 
-# Create your models here.
+class Partaker(models.Model):
+    name = models.CharField(max_length=64)
+    event = models.ForeignKey("event.Event", on_delete=models.CASCADE)
+
+
+class Player(Partaker):
+    discord = models.CharField(max_length=64)
+    Telegram = models.CharField(max_length=64)
+
+
+class Team(Partaker):
+    leader = models.ForeignKey("self", on_delete=models.CASCADE)
+    members = models.ManyToManyField("self")
