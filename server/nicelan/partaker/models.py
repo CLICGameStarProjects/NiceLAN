ad356a8622a20c22c334ebca6e8b15fa7a412493
Marwa-rez <rezziki.marwa@gmail.com>
2022-11-27 12:40:06 +0100

    17-create-base-for-players

--- server/nicelan/partaker/models.py
+++ server/nicelan/partaker/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Partaker(models.Model):
     name = models.CharField(max_length=64)
     event = models.ForeignKey("event.Event", on_delete=models.CASCADE)
+    activities = models.ManyToManyField("activity.Activity")
 
     @property
     def is_player(self):
@@ -15,6 +16,12 @@ class Player(Partaker):
     Telegram = models.CharField(max_length=64, blank=True)
 
 
+
 class Team(Partaker):
     leader = models.ForeignKey("self", on_delete=models.CASCADE)
     members = models.ManyToManyField("self")
+
+
+# sur-roles de player : PAS MTN
+#class Manager(Player):
+#    activity = models.ForeignKey("activity.Activity", on_delete=models.CASCADE)
